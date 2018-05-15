7e33a1460e45ad2207bf23b87527221f0eb706a5
TeodorVecerdi <teo.vecerdi@gmail.com>
2018-05-15 17:47:50 +0300

    test

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import re
 TOKEN = 'NDQ1NjUwNDc2NDM2ODE1ODkz.Ddtj7Q.9AmZpLZxE1NnyjUq-R9gXIxntCM'
 nick = 'vqgXph15'
 
-client = discord.Client()
 
 def createSession():
 	body = {
@@ -35,28 +34,69 @@ def sendMessage(message):
 
 talking = {}
 commands = {}
+profile = {}
+client = discord.Client()
+currentBot = 'Cleverbot'
+profile['TicTacToe'] = open('tictactoe_profile.png', 'rb').read()
+profile['Cleverbot'] = open('cleverbot_profile.png', 'rb').read()
+
+def cmd_switch(message):
+	if currentBot == 'Cleverbot':
+		currentBot = 'TicTacToe'
+	else:
+		currentBot = 'Cleverbot'
+
+	return 'Switched to `{}`. Type `cvb switch` to switch again'
 
 def cmd_talk(message):
 	talking[str(message.author)] = True
 	print(talking)
 	return 'Started talking to Cleverbot. To stop type `cvb stop`'
-	
+
 def cmd_stop(message):
 	talking[str(message.author)] = False
 	print(talking)
 	return 'Stopped talking to Cleverbot. To talk again type `cvb talk`'
 
 def cmd_help(message):
-	return 'All Cleverbot commands start with cvb\n`cvb help` - show this message\n`cvb talk` - start talking with Cleverbot\n`cvb stop` - stop talking to Cleverbot\n`cvb info` - print information about Cleverbot'
+	retMsg = 'All Cleverbot commands start with cvb:\n'
+	for command in commands:
+		retMsg += '`cvb {}` - {}\n'.format(command['cmd'], command['desc'])
+	return '\n`cvb help` - show this message\n`cvb talk` - start talking with Cleverbot\n`cvb stop` - stop talking to Cleverbot\n`cvb info` - print information about Cleverbot'
 
 def cmd_info(message):
 	return 'Cleverbot is a chatterbot web application that uses an artificial intelligence algorithm to have conversations with humans.'
 
 def init():
-	commands['talk'] = cmd_talk
-	commands['stop'] = cmd_stop
-	commands['help'] = cmd_help
-	commands['info'] = cmd_info
+	commands['talk'] = {}
+	commands['talk']['func'] = cmd_talk
+	commands['talk']['cmd'] = 'talk'
+	commands['talk']['desc'] = 'start talking with Cleverbot'
+	commands['talk']['bot'] = 'Cleverbot'
+
+	commands['stop'] = {}
+	commands['stop']['func'] = cmd_stop
+	commands['stop']['cmd'] = 'stop'
+	commands['stop']['desc'] = 'stop talking to Cleverbot'
+	commands['stop']['bot'] = 'Cleverbot'
+
+	commands['help'] = {}
+	commands['help']['func'] = cmd_help
+	commands['help']['cmd'] = 'help'
+	commands['help']['desc'] = 'print this help message'
+	commands['help']['bot'] = 'General'
+
+	commands['info'] = {}
+	commands['info']['func'] = cmd_info
+	commands['info']['cmd'] = 'info'
+	commands['info']['desc'] = 'details about Cleverbot'
+	commands['info']['bot'] = 'Cleverbot'
+
+	commands['switch'] = {}
+	commands['switch']['func'] = cmd_switch
+	commands['switch']['cmd'] = 'switch'
+	commands['switch']['desc'] = 'switch between Cleverbot and TicTacToe'
+	commands['switch']['bot'] = 'General'
 
 
 @client.event
@@ -70,7 +110,13 @@ async def on_message(message):
 			command = ''
 			try:
 				command = re.match('cvb (.*[a-zA-Z0-9])', message.content).group(1)
-				resp = commands[command](message)
+				if commands[command]['bot'] == 'General' or commands[command]['bot'] == currentBot:
+					resp = commands[command]['func'](message)
+					if command == 'switch':
+						await client.change_nickname(discord.Server.me, currentBot)
+						await client.edit_profile(avatar=profile[currentBot])
+				else:
+					await client.send_message(message.channel, 'Command `cvb {}` is not available for the current bot. Type `cvb switch` to switch to another bot and try again.')
 				if resp != '':
 					await client.send_message(message.channel, resp)
 			except:
